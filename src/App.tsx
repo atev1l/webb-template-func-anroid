f1c4a0b1a3e8fb458bf58f4d7db85e5d3b5ccefa
atev1l <killfreedom@yandex.ru>
2024-03-14 23:14:57 +0300

    f2 msg

--- src/App.tsx
+++ src/App.tsx
@@ -130,64 +130,159 @@ function App() {
     }
   };
 
+  // <div className="App">
+  //   <div className="display">
+  //     <div className="brdr">
+  //       status worker: { workerApi }
+  //     </div>
+  //     <div className="brdr">
+  //       { localValue }
+  //       <button onClick={() => changeLocal(true, 'local', 'true')}>Add to local</button>
+  //       <button onClick={() => changeLocal(false, 'local', 'false')}>Remove from local</button>
+  //     </div>
+  //     <div className="brdr">
+  //       { sessionValue }
+  //       <button onClick={() => changeSession(true, 'session', 'true')}>Add to session</button>
+  //       <button onClick={() => changeSession(false, 'session', 'false')}>Remove from session</button>
+  //     </div>
+  //     11111
+  //     <AvatarEditor
+  //         image={image}
+  //         width={250}
+  //         height={250}
+  //         border={50}
+  //         color={[255, 255, 255, 0.6]} // RGBA
+  //         scale={1.2}
+  //         rotate={0}
+  //     />
+  //     <Avatar googleId="118096717852922241760" size="100" round={true} />
+  //     <div className="brdr">
+  //       <input type="file" accept="image/*" onChange={handleImageChange} capture="user" />
+  //       {image && (
+  //           <div>
+  //             <img src={image} alt="Uploaded" style={{ maxWidth: '100%', maxHeight: '300px' }} />
+  //             <p>Image uploaded!</p>
+  //           </div>
+  //       )}
+  //     </div>
+  //     <div className="brdr">
+  //       { nameDeviceBLT }
+  //       <button onClick={() => checkBlue()}>check blt</button>
+  //     </div>
+  //     <div className="brdr">
+  //       <div>{ btr.batteryLevel }</div>
+  //       <button onClick={() => checkBatteryLevel()}>check батарейки</button>
+  //     </div>
+  //
+  //     <div className="brdr">
+  //       <input type="file" onChange={handleFileChange} />
+  //       {fileContent && <div>File Content: {fileContent}</div>}
+  //     </div>
+  //     <div className="brdr">
+  //       audio
+  //       <audio controls={true}>
+  //         <source src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3" type="audio/mpeg" />
+  //         Your browser does not support the audio element.
+  //       </audio>
+  //     </div>
+  //   </div>
+  //
+  // </div>
   return (
-    <div className="App">
-      <div className="display">
-        <div className="brdr">
-          status worker: { workerApi }
-        </div>
-        <div className="brdr">
-          { localValue }
-          <button onClick={() => changeLocal(true, 'local', 'true')}>Add to local</button>
-          <button onClick={() => changeLocal(false, 'local', 'false')}>Remove from local</button>
-        </div>
-        <div className="brdr">
-          { sessionValue }
-          <button onClick={() => changeSession(true, 'session', 'true')}>Add to session</button>
-          <button onClick={() => changeSession(false, 'session', 'false')}>Remove from session</button>
-        </div>
-        11111
-        <AvatarEditor
-            image={image}
-            width={250}
-            height={250}
-            border={50}
-            color={[255, 255, 255, 0.6]} // RGBA
-            scale={1.2}
-            rotate={0}
-        />
-        <Avatar googleId="118096717852922241760" size="100" round={true} />
-        <div className="brdr">
-          <input type="file" accept="image/*" onChange={handleImageChange} capture="user" />
-          {image && (
-              <div>
-                <img src={image} alt="Uploaded" style={{ maxWidth: '100%', maxHeight: '300px' }} />
-                <p>Image uploaded!</p>
-              </div>
-          )}
-        </div>
-        <div className="brdr">
-          { nameDeviceBLT }
-          <button onClick={() => checkBlue()}>check blt</button>
-        </div>
-        <div className="brdr">
-          <div>{ btr.batteryLevel }</div>
-          <button onClick={() => checkBatteryLevel()}>check батарейки</button>
-        </div>
-
-        <div className="brdr">
-          <input type="file" onChange={handleFileChange} />
-          {fileContent && <div>File Content: {fileContent}</div>}
-        </div>
-        <div className="brdr">
-          audio
-          <audio controls={true}>
-            <source src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3" type="audio/mpeg" />
-            Your browser does not support the audio element.
-          </audio>
-        </div>
+    <div>
+      <div style={{fontSize: '92px'}}>
+        EXMAPLE
       </div>
-
+      <div style={{fontSize: '92px'}}>
+        EXMAPLE
+      </div>
+      <div style={{fontSize: '92px'}}>
+        EXMAPLE
+      </div>
+      <div style={{fontSize: '92px'}}>
+        EXMAPLE
+      </div>
+      <div style={{fontSize: '92px'}}>
+        EXMAPLE
+      </div>
+      <div style={{fontSize: '92px'}}>
+        EXMAPLE
+      </div>
+      <div style={{fontSize: '92px'}}>
+        EXMAPLE
+      </div>
+      <div style={{fontSize: '92px'}}>
+        EXMAPLE
+      </div>
+      <div style={{fontSize: '92px'}}>
+        EXMAPLE
+      </div>
+      <div style={{fontSize: '92px'}}>
+        EXMAPLE
+      </div>
+      <div style={{fontSize: '92px'}}>
+        EXMAPLE
+      </div>
+      <div style={{fontSize: '92px'}}>
+        EXMAPLE
+      </div>
+      <div style={{fontSize: '92px'}}>
+        EXMAPLE
+      </div>
+      <div style={{fontSize: '92px'}}>
+        EXMAPLE
+      </div>
+      <div style={{fontSize: '92px'}}>
+        EXMAPLE
+      </div>
+      <div style={{fontSize: '92px'}}>
+        EXMAPLE
+      </div>
+      <div style={{fontSize: '92px'}}>
+        EXMAPLE
+      </div>
+      <div style={{fontSize: '92px'}}>
+        EXMAPLE
+      </div>
+      <div style={{fontSize: '92px'}}>
+        EXMAPLE
+      </div>
+      <div style={{fontSize: '92px'}}>
+        EXMAPLE
+      </div>
+      <div style={{fontSize: '92px'}}>
+        EXMAPLE
+      </div>
+      <div style={{fontSize: '92px'}}>
+        EXMAPLE
+      </div>
+      <div style={{fontSize: '92px'}}>
+        EXMAPLE
+      </div>
+      <div style={{fontSize: '92px'}}>
+        EXMAPLE
+      </div>
+      <div style={{fontSize: '92px'}}>
+        EXMAPLE
+      </div>
+      <div style={{fontSize: '92px'}}>
+        EXMAPLE
+      </div>
+      <div style={{fontSize: '92px'}}>
+        EXMAPLE
+      </div>
+      <div style={{fontSize: '92px'}}>
+        EXMAPLE
+      </div>
+      <div style={{fontSize: '92px'}}>
+        EXMAPLE
+      </div>
+      <div style={{fontSize: '92px'}}>
+        EXMAPLE
+      </div>
+      <div style={{fontSize: '92px'}}>
+        EXMAPLE
+      </div>
     </div>
   );
 }
